38de1a487e9eb6cbef6c6773b20a25dee6dcc4e3
Stefanus <stephanuswilfrid@gmail.com>
2024-09-27 15:24:46 +1000

    work on schema

---
+++ internal/schema/attachment.go
@@ -0,0 +1,14 @@
+package schema
+
+import (
+	"github.com/google/uuid"
+)
+
+type Attachment struct {
+	ID           uuid.UUID  `json:"id" gorm:"primaryKey"`
+	URL          string     `json:"url" gorm:"type:text;not null"`
+	AssignmentID *uuid.UUID `json:"assignment_id" gorm:""`
+	MaterialID   *uuid.UUID `json:"material_id" gorm:""`
+	SubmissionID *uuid.UUID `json:"submission_id" gorm:""`
+	Description  string     `json:"description" gorm:"type:varchar(1000)"`
+}
