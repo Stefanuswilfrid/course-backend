4bd9b029d2e60442d1fe9d83c568997c7c43c873
Stefanus <stephanuswilfrid@gmail.com>
2024-10-02 22:34:43 +1000

    submission

---
+++ internal/domain/submission/dto.go
@@ -0,0 +1,20 @@
+package submission
+
+import (
+	"mime/multipart"
+)
+
+type CreateSubmissionRequest struct {
+	AssignmentID string                  `form:"assignment_id" binding:"required" `
+	Content      string                  `form:"content" `
+	Attachments  []*multipart.FileHeader `form:"attachments"`
+}
+
+type UpdateSubmissionRequest struct {
+	Content     *string                 `form:"content,omitempty"`
+	Attachments []*multipart.FileHeader `form:"attachments,omitempty"`
+}
+
+type GradeSubmissionRequest struct {
+	Grade float64 `json:"grade" binding:"required,min=0,max=100"`
+}
