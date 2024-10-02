2253c1c1572d19c240d21bd2174bafef06b7375c
Stefanus <stephanuswilfrid@gmail.com>
2024-10-02 22:05:29 +1000

    material

---
+++ internal/domain/material/error.go
@@ -0,0 +1,45 @@
+package material
+
+import (
+	"net/http"
+
+	"github.com/Stefanuswilfrid/course-backend/internal/apierror"
+)
+
+var (
+	ErrMaterialNotFound = apierror.NewApiErrorBuilder().
+				WithHttpStatus(http.StatusNotFound).
+				WithMessage("MATERIAL_NOT_FOUND")
+
+	ErrInvalidCourseData = apierror.NewApiErrorBuilder().
+				WithHttpStatus(http.StatusBadRequest).
+				WithMessage("INVALID_COURSE_DATA")
+
+	ErrNotOwnerAccess = apierror.NewApiErrorBuilder().
+				WithHttpStatus(http.StatusUnauthorized).
+				WithMessage("NOT_YOUR_COURSE")
+
+	ErrUnauthorizedAccess = apierror.NewApiErrorBuilder().
+				WithHttpStatus(http.StatusUnauthorized).
+				WithMessage("UNAUTHORIZED_ACCESS")
+
+	ErrForbiddenOperation = apierror.NewApiErrorBuilder().
+				WithHttpStatus(http.StatusForbidden).
+				WithMessage("FORBIDDEN_OPERATION")
+
+	ErrDatabaseOperationFail = apierror.NewApiErrorBuilder().
+					WithHttpStatus(http.StatusInternalServerError).
+					WithMessage("DATABASE_OPERATION_FAILED")
+
+	ErrS3UploadFail = apierror.NewApiErrorBuilder().
+			WithHttpStatus(http.StatusInternalServerError).
+			WithMessage("S3_UPLOAD_FAILED")
+
+	ErrUUIDGenerationFail = apierror.NewApiErrorBuilder().
+				WithHttpStatus(http.StatusInternalServerError).
+				WithMessage("UUID_GENERATION_FAILED")
+
+	ErrEditConflict = apierror.NewApiErrorBuilder().
+			WithHttpStatus(http.StatusConflict).
+			WithMessage("EDIT_CONFLICT")
+)
